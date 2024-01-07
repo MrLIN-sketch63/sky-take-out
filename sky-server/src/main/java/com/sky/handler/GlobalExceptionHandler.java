b36c6ffa3e31f341050c3ca9c6568d9ca9d623bd
Ziyang Lin <2761137L@student.gla.ac.uk>
2024-01-07 22:05:41 +0800

    Insert employee part has completed.

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,19 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        // Duplicate entry 'zhangsan' for key 'employee.idx_username  错误信息示例
+        String message = ex.getMessage();   //获取错误信息
+        if (message.contains("Duplicate entry")) {
+            String[] split = message.split(" ");   //截取错误信息
+            String username = split[2];   //  重复的用户名位于错误信息中的第三个位置
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
+    }
 }
