50c17d72f23c98db36e7f878bd17ed5a7ca326ca
Ziyang Lin <2761137L@student.gla.ac.uk>
2024-01-11 11:18:26 +0800

    Insert dish functionality has been completed.

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,26 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 用于创建AliOssUtil对象
+ */
+
+@Configuration
+@Slf4j
+public class OssConfiguration { //该类创建了AliOssUtil这个工具类的对象并把这个对象赋值后交给ioc容器管理
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象: {}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
